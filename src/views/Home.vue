821b7c4e78f6165079083a908ee5e785dc8b2ae0
falcodev <renatomacedomonteiro@gmail.com>
2021-03-27 20:03:10 -0300

    Firebase rodando

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-<br>
+<div class="home">
+  <img alt="Vue logo" src="../assets/logo.png">
+  <br>
   <span id="hora">{{hora}}</span>
-    <HelloWorld msg="Essa é a versão Digital"/>
-  </div>
+  <HelloWorld msg="Essa é a versão Digital" />
+</div>
 </template>
 
 <script>
@@ -17,14 +17,15 @@ export default {
     HelloWorld,
   },
   data() {
-  return{
-  horario:null,
-  }
+    return {
+      horario: null,
+    }
 
-},
+  },
 
-  mounted(){
-    setInterval(myTimer,60);
+  mounted() {
+    setInterval(myTimer, 60);
+
     function myTimer() {
       var dia_atual = new Date();
       // não consegui remover o innerhtml usando o vue js, pois não consegui a conexão dentro do mounted com o html
@@ -32,37 +33,37 @@ export default {
     }
 
 
-/*
-colocando o valor das horas na marra, visto que não sabia da existencia do "toLocaleTimeString()"
-      setInterval(relogio,60);
-      // Progamação do relógio
-      function relogio(){
-        var dia_atual = new Date();
-        if (dia_atual.getSeconds() > 10) {
-          this.horario = dia_atual.getHours() +':'+ dia_atual.getMinutes()+':'+ dia_atual.getSeconds();
-        }else {
-          this.horario = dia_atual.getHours() +':'+ dia_atual.getMinutes()+':'+ '0' + dia_atual.getSeconds();
-        }
-      }
-*/
+    /*
+    colocando o valor das horas na marra, visto que não sabia da existencia do "toLocaleTimeString()"
+          setInterval(relogio,60);
+          // Progamação do relógio
+          function relogio(){
+            var dia_atual = new Date();
+            if (dia_atual.getSeconds() > 10) {
+              this.horario = dia_atual.getHours() +':'+ dia_atual.getMinutes()+':'+ dia_atual.getSeconds();
+            }else {
+              this.horario = dia_atual.getHours() +':'+ dia_atual.getMinutes()+':'+ '0' + dia_atual.getSeconds();
+            }
+          }
+    */
   }
 };
-
-
 </script>
 <style>
-.centralizando{
+.centralizando {
   align-items: center;
   width: 100%;
   background-color: red;
 }
-  span {
 
-    text-align: center;
-    font-size:65pt;
-    font-family: clockicons,Electrolize,Iceberg,sans-serif;
+span {
+
+  text-align: center;
+  font-size: 65pt;
+  font-family: clockicons, Electrolize, Iceberg, sans-serif;
 }
-h2{
+
+h2 {
   font-size: 65pt;
 }
 </style>
