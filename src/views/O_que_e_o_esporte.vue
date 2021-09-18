158a6f0197e4c8ea92b8d0758d50bf00242f9c01
falcodev <renatomacedomonteiro@gmail.com>
2021-09-18 16:10:41 -0300

    Adição de um artigo

--- src/views/O_que_e_o_esporte.vue
+++ src/views/O_que_e_o_esporte.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="artigo">
     <h1>O que é o esporte</h1>
+<p>
+O esporte teve um grande crescimento no decorrer dos tempos, mesmo assim muitas pessoas costumam classificar  o esporte com lazer ou atividade física ou até  mesmo um ato de ganhar dinheiro.
+Porém esse senso comum é muito superficial e não contempla a definição do esporte como um todo.</p>
+<p>
+
+Aprofundando sobre o conceito de  esporte lazer e atividades físicas. A distinção aqui é bem sutil e interpretativa, há situações em que a mesma atividade pode ser considerada um mero lazer(brincadeira), como uma atividade física esportiva.
+O exemplo aqui é um grupo de crianças que estão jogando no pátio da escola, no intervalo das aulas, podemos dizer que ali é apenas uma diversão entre alunos e colegas.
+Já quando esse mesmo grupo de jovens, praticam sobre normas técnicas, podemos considerar uma atividade esportiva.
+Mesmo assim, há situações em que mesmo o indivíduo executando prática esportiva, não é considerada o esporte, como em vários casos de paraquedismo, surf, escaladas.
+Dito isso, entram em cena dois elementos chaves do esporte. A arbitragem e o público.
+</p>
+  <p>
+ 	A arbitragem na verdade é só a ponta da cadeia, ela é a responsável por validar os resultados e permitir que tenham resultados comparáveis com outros jogos. Para isso, todo um trabalho deve ser feito, pelas federações, e organizações para criação dos regulamentos, bem como a promoção do evento.
+  </p>
+  <p>No constante do público, esses são o motivo do espetáculo em si.
+    Eles promovem um sentimento de importância para os atletas e o reconhecimento pelo árduo trabalho feito pelos mesmos.
+    Sem isso, seria economicamente inviável a prática esportiva em alto nível.
+	O senso comum também diz que o esporte necessariamente é uma atividade física, isso no entanto é um equívoco.
+  Um grande exemplo disso é o xadrez, que  a habilidade motora exigida não vai além de ser capaz de mover as peças sem derrubar as outras do tabuleiro.
+   Apesar da possibilidade da partida ser longa e ser cansativa emocionalmente e fisicamente, a concentração e outras habilidades cognitivas são os fatores determinantes nesse jogo.
+   Assim, xadrez não envolve o uso de habilidades motoras relativamente complexas, nem esforço físico vigoroso, mesmo assim é considerado mundialmente como esporte.
+    Nesse sentido, temos outros esportes, como a renomada  fórmula 1.
+    Além desses tradicionais,outros ESPORTES tem ganhado cada vez mais atenção do público, e apesar de terem  uma classificação diferente, ainda sim, são esportes.
+    Estamos falando dos E-Sports.
+  </p>
+  <p>
+	Nessa categoria, destaca-se principalmente alguns jogos de desktop, como,Overwatch, Rainbow Six,PUB,Rocket League,League of Legends e DOTA 2.
+   Este último, só para citar o exemplo da importância econômica, movimentou mais de 40 milhões de premiação, o que já supera grandes competições tais como Copa do Brasil e a Libertadores da América JUNTOS.
+   Além disso, o jogo tem série própria produzida pela netflix e relacionado a história dos personagens.
+  </p>
+  <p>
+	Por fim, percebemos que o esporte é muito mais que uma brincadeira, uma competição, regulamento, espetáculo.
+  Na verdade ele é justamente tudo isso, interligado, podemos dizer que: “Esporte é uma atividade competitiva institucionalizada que envolve esforço físico vigoroso ou o uso de habilidades motoras relativamente complexas, por indivíduos, cuja participação é motivada por uma combinação de fatores intrínsecos e
+extrínsecos”.
+</p>
+
   </div>
 </template>
