391b06f35c269507023fffecfbfe3742a6caed86
Renato Macedo Monteiro <renatomacedomonteiro@gmail.com>
2022-09-07 12:15:45 -0300

    Atualização de campeonatos e ajuste no rss

--- src/components/Lista_de_campeonato_bt.vue
+++ src/components/Lista_de_campeonato_bt.vue
@@ -15,14 +15,14 @@ export default {
 
   return{
   info:[
-      {categoria:'bt',nome_torneio:'Circuito Santa Praia - 4ª Etapa Vento ',data:'1/09/22 a 04/09/22', nivel:'Iniciante,c,b,a,pro,(Masculino,feminino misto)',local:'Fortaleza Santa Praia', contato:'https://www.circuitosantapraia.com.br/'},
-      {categoria:'bt',nome_torneio:'WBS Brazil Celebration ',data:'09/09 a 11/09/2022', nivel:'Iniciantes',local:'Fortaleza - CE ', contato:'8532531803'},
-      {categoria:'bt',nome_torneio:'3° Preá open Volei e Beach tennis ',data:'24/09/22 a 25/09/22', nivel:'Masculino Feminino e 70+', local:'Arena Acp - Preá', contato: "(88)9.99730-5466"},
+      {categoria:'bt',nome_torneio:'WBS Brazil Celebration ',data:'09/09/22 a 11/09/2022', nivel:'Iniciantes',local:'Fortaleza - CE ', contato:'8532531803'},
+      {categoria:'bt',nome_torneio:'1° Ano Arena Original',data:'16/09/22 a 18/09/22', nivel:'Principiantes,Iniciante,intermediária e mista', local:'Arena original, sede azevedo bolão - Fortaleza', contato: "(85)9.9174-2084"},
       {categoria:'bt',nome_torneio:'8° Etapa campeonato cearense de beach tennis',data:'20/09/2022 a 25/09/2022', nivel:'Iniciante,c,b,a,pro,(Masculino,feminino misto)',local:'Fortaleza A12- CE ', contato:'85 98656 2563'},
+      {categoria:'bt',nome_torneio:'3° Preá open Volei e Beach tennis ',data:'24/09/22 a 25/09/22', nivel:'Masculino Feminino e 70+', local:'Arena Acp - Preá', contato: "(88)9.99730-5466"},
       {categoria:'bt',nome_torneio:'9° Etapa campeonato cearense de beach tennis ',data:'6/10/22 a 09/10/22', nivel:'Iniciante,c,b,a,pro,(Masculino,feminino misto)', local:'RBBT - Fortaleza', contato: "(85)9.8146-4854"},
       {categoria:'bt',nome_torneio:'10° Etapa campeonato cearense de beach tennis ',data:'18/10/22 a 23/10/22', nivel:'Iniciante,c,b,a,pro,(Masculino,feminino misto)', local:'Arena set - Fortaleza', contato: "(85)9.8146-4854"},
       {categoria:'bt',nome_torneio:'11° Etapa campeonato cearense de beach tennis ',data:'3/11/22 a 06/11/22', nivel:'Iniciante,c,b,a,pro,(Masculino,feminino misto)', local:'Spine - Fortaleza', contato: "(85)9.8146-4854"},
-      {categoria:'bt',nome_torneio:'12° Etapa campeonato cearense de beach tennis ',data:'1/12/22 a 04/12/22', nivel:'Iniciante,c,b,a,pro,(Masculino,feminino misto)', local:'MArabeach - Fortaleza', contato: "(85)9.8146-4854"},
+      {categoria:'bt',nome_torneio:'12° Etapa campeonato cearense de beach tennis ',data:'1/12/22 a 04/12/22', nivel:'Iniciante,c,b,a,pro,(Masculino,feminino misto)', local:'Marabeach - Fortaleza', contato: "(85)9.8146-4854"},
 
 
 
@@ -48,6 +48,7 @@ export default {
 {categoria:'bt',nome_torneio:'3º Etapa Campeonato Cearense de Beach tennis ',data:'7/04/22 a 9/04/22', nivel:'subs 12,14, 40+ 50+,Iniciante,c,b,a,pro,(Masculino,feminino,misto)',local:'Fortaleza Villa Beach tennis', contato:'85 9.9988-7328'},
 {categoria:'bt',nome_torneio:'2° Preá Beach Tennis ',data:'12/09/21 a 15/09/21', nivel:'Iniciante,c,b,a,pro,+40,+50(Masculino,feminino e misto)',local:'Preá-ce'},
 {categoria:'BT',nome_torneio:'Mundo unifor 2021 -',data:'02/10/21', nivel:'Escolar  sub 17-', local:'Fortaleza Cactus', contato: "(85)3477-3143"},
+{categoria:'bt',nome_torneio:'Circuito Santa Praia - 4ª Etapa Vento ',data:'1/09/22 a 04/09/22', nivel:'Iniciante,c,b,a,pro,(Masculino,feminino misto)',local:'Fortaleza Santa Praia', contato:'https://www.circuitosantapraia.com.br/'},
   }
 ]*/
 
--- src/components/Lista_de_campeonato_volei.vue
+++ src/components/Lista_de_campeonato_volei.vue
@@ -18,18 +18,19 @@ export default {
 
   return{
   info:[
-    {categoria:'Volei',nome_torneio:'3° Festival Metropolitano  do ceara 4 etapa',data:'27/08/22 a 28/08/22', nivel:'Masculino e feminino', local:'Indefinido', contato: "(85)9.9814-9058"},
+    {categoria:'Volei de praia',nome_torneio:'2° Etapa circuito cearense volei ',data:'09/09/22 a 11/09/22', nivel:'Sub 15, sub 17 e sub 21 masculino e feminino', local:'Aterro praia de iracema wbs', contato: "(85)3456-3006"},
     {categoria:'Volei de praia',nome_torneio:'2° Pé na areia Arena MM 2022 ',data:'16/09/22 a 18/09/22', nivel:'Masculino,feminino e misto Iniciante, intermediario e avançado', local:'Arena MM', contato: "(85)9.999624-7015"},
     {categoria:'Volei de praia',nome_torneio:'3° Preá open Volei e Beach tennis ',data:'24/09/22 a 25/09/22', nivel:'Masculino Feminino e 70+', local:'Arena Acp - Preá', contato: "(88)9.99730-5466"},
-
+    {categoria:'Volei',nome_torneio:'3° Torneio Alex Ponciano',data:'08/10/22 a 09/10/22', nivel:'Não informado', local:'Não informado - Fortaleza', contato: "(85)9.8800-5522"},
+    
 
 
 ],
 historico:[
-  {categoria:'Volei',nome_torneio:'3° Festival Metropolitano  do ceara 2 etapa',data:'21/05/22 a 22/05/22', nivel:'Masculino e feminino', local:'Indefinido', contato: "(85)9.9814-9058"},
+  {categoria:'Volei',nome_torneio:'3° Festival Metropolitano  do ceara 1 etapa',data:'26/03/22 a 27/03/22', nivel:'Masculino e feminino', local:'Indefinido', contato: "(85)9.9814-9058"},
   {categoria:'Volei',nome_torneio:'3° Festival Metropolitano  do ceara 2 etapa',data:'21/05/22 a 22/05/22', nivel:'Masculino e feminino', local:'Indefinido', contato: "(85)9.9814-9058"},
   {categoria:'Volei',nome_torneio:'3° Festival Metropolitano  do ceara 3 etapa',data:'23/07/22 a 24/07/22', nivel:'Masculino e feminino', local:'Indefinido', contato: "(85)9.9814-9058"},
-  {categoria:'Volei',nome_torneio:'3° Festival Metropolitano  do ceara 1 etapa',data:'26/03/22 a 27/03/22', nivel:'Masculino e feminino', local:'Indefinido', contato: "(85)9.9814-9058"},
+  {categoria:'Volei',nome_torneio:'3° Festival Metropolitano  do ceara 4 etapa',data:'27/08/22 a 28/08/22', nivel:'Masculino e feminino', local:'Indefinido', contato: "(85)9.9814-9058"},
   {categoria:'Volei de Praia',nome_torneio:'Festival to na praia ',data:'29/04/22 a 01/05/22 ', nivel:'open',local:'Paracuru Ronco do mar',contato:""},
   {categoria:'Volei Praia',nome_torneio:'Open one clube',data:'09/04/22 e 10/04/22', nivel:'Aberto', local:'Maranguape oneclube', contato: "(85)9.98153-1301"},
   {categoria:'Volei Praia',nome_torneio:'Campeontao cearense de Vôlei de praia',data:'11/12/21 e 12/12/21', nivel:'Masculino Aberto', local:'Fortaleza CFO', contato: "(85)9.3456-3006"},
